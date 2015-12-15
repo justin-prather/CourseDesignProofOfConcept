b0472772770da318497c473bb81d4d6a580302a8
Justin Prather <justinprather@mac.com>
2015-12-14 21:44:54 -0800

    measurment working, highlight selected working, swap dimensions if in wrong order working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,40 +3,23 @@ var canvas, stage;
 var scale, railLength;
 var template;
 
-var activeRail;
+var activeRail = null;
 
 var rails = [];
 
 var dblClick = false;
-
-var setRingParams = function(){
-	var ringWidth = $('#widthInput').val();
-	var ringHeight = $('#heightInput').val();
-	railLength = $('#railLength').val();
-	var ratio = ringHeight/ringWidth;
-
-	var windowWidth = $('.container').width();
-	canvas.width = windowWidth;
-
-	scale = windowWidth/ringWidth;
-	console.log(railLength*scale);
-
-	canvas.height = windowWidth*ratio;
-
-	var length = rails.length;
-	for ( var i = 0; i < length; i++ ){
-		rails.push(newRail("Purple", "red", rails[i].x, rails[i].y));
-		stage.removeChild(rails[i]);
-		rails.splice( i, 1 );
-	}
-	update = true;
-};
+/*
+	0 = normal
+	1 = measure
+*/
+var mode = 0;
 
 $(function(){
 
 	template = Handlebars.compile($('#rails-template').html());
 
 	$('#submit').click( setRingParams );
+	$('#measure').click( event, measure );
 	
 	canvas = document.getElementById('demoCanvas');
 	canvas.addEventListener("mousewheel", MouseWheelHandler, false);
@@ -48,7 +31,12 @@ $(function(){
 		if( !dblClick ){
 			dblClick = true;
 			setTimeout(function(){ dblClick = false}, 200);
-			activeRail = null;
+			
+			if( mode == 0 ){
+				if ( activeRail != null ) activeRail.children[0].visible = false;
+				activeRail = null;
+				update = true;
+			}
 		}
 		else{
 			rails.push(newRail("Purple", "red", event.stageX, event.stageY));
@@ -79,46 +67,108 @@ function tick(event) {
 	}
 }
 
+var setRingParams = function(){
+	var ringWidth = $('#widthInput').val();
+	var ringHeight = $('#heightInput').val();
+
+	if( ringHeight > ringWidth ){
+		var temp = ringWidth;
+		ringWidth = ringHeight;
+		ringHeight = temp;
+		$('#widthInput').val(ringWidth);
+		$('#heightInput').val(ringHeight);
+	}
+
+	railLength = $('#railLength').val();
+	var ratio = ringHeight/ringWidth;
+
+	var windowWidth = $('.container').width();
+	canvas.width = windowWidth;
+
+	scale = windowWidth/ringWidth;
+	console.log(railLength*scale);
+
+	canvas.height = windowWidth*ratio;
+
+	var length = rails.length;
+	for ( var i = 0; i < length; i++ ){
+		rails.push(newRail("Purple", "red", rails[i].x, rails[i].y));
+		stage.removeChild(rails[i]);
+		rails.splice( i, 1 );
+	}
+	update = true;
+};
+
 var newRail = function(color, selectColor, x, y) {
 	var rect = new createjs.Shape();
 	var circle = new createjs.Shape();
+	var container = new createjs.Container();
+	container.mouseChildren = false;
 
 	rect.graphics.beginFill(color).drawRect(0, 0, railLength*scale, 1.5*scale);
-	circle.graphics.beginFill(selectColor).drawCircle( 0, 0, railLength*scale/2 );
+	circle.graphics.beginRadialGradientFill([selectColor, 'rgba(255, 255, 255, 0)'], [0.9,0.1], 0, 0, 0, 0, 0, railLength*scale*1.2)
+		.drawCircle( 0, 0, railLength*scale/2 );
 
-	circle.alpha = 0.3;
-	circle.visible = false;
+	circle.alpha = 0.9;
 
 	rect.regX = railLength*scale/2;
 	rect.regY = scale;
 
-	rect.x = circle.x = x;
-	rect.y = circle.y = y;
+	container.x = x;
+	container.y = y;
 
 	rect.rotation = 90;
 
-	rect.on("mousedown", function(evt){
+	container.addChild(circle);
+	container.addChild(rect);
+
+	container.on("mousedown", function(evt){
 		this.offset = {x: this.x - evt.stageX, y: this.y - evt.stageY};
 	});
 
-	rect.on("click", function(evt){
-		console.log('hello');
-		activeRail = this;
+	container.on("click", function(evt){
+		if( mode == 0 ){ 
+			activeRail = this;
+			this.children[0].visible = true;
+			update = true;
+		}
+		else if( mode == 1 ){
+			if ( activeRail == null || activeRail == undefined ){
+				activeRail = this;
+				this.children[0].visible = true;
+				update = true;
+			} else {
+				var startX = activeRail.x;
+				var startY = activeRail.y;
+				var endX = this.x;
+				var endY = this.y;
+
+				var distance = Math.abs( hypoteneus( endX - startX, endY - startY ) );
+
+				$('#measurment').toggleClass('hidden').text(parseFloat(distance / scale).toFixed(2) + ' Feet' );
+				measure();
+			}
+		}
 	});
 
 	// the pressmove event is dispatched when the mouse moves after a mousedown on the target until the mouse is released.
-	rect.on("pressmove", function (evt) {
+	container.on("pressmove", function (evt) {
 		this.x = evt.stageX + this.offset.x;
 		this.y = evt.stageY + this.offset.y;
 		// indicate that the stage should be updated on the next tick:
 		update = true;
 	});
 
-	stage.addChild(circle);
-	stage.addChild(rect);
+	stage.addChild(container);
 
-	return rect;
+	activeRail = container;
+
+	return container;
 }
+
+var hypoteneus = function( x, y ){
+	return Math.sqrt( (x*x) + (y*y) );
+};
 
 var updateList = function(){
 	var data = {};
@@ -145,3 +195,15 @@ var MouseWheelHandler = function(event){
 	event.preventDefault();
 	return false;
 };
+
+var measure = function(){
+	if ( mode != 1 ){
+		mode = 1;
+		$('#measure').addClass('btn-success');
+		$('#measure').removeClass('btn-default');
+	} else {
+		mode = 0;
+		$('#measure').removeClass('btn-success');
+		$('#measure').addClass('btn-default');
+		}
+}
